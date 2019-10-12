3c399d32d1c0901a73e37a18960b47ef31edd839
yvano berthol <yvanoberthol@gmail.com>
2019-10-12 12:43:26 +0200

    ajout de la classe AccesDeniedHandle et User_checker

--- src/Security/AccesDeniedHandler.php
+++ src/Security/AccesDeniedHandler.php
@@ -13,9 +13,27 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class AccesDeniedHandler implements AccessDeniedHandlerInterface
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * AccesDeniedHandler constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
 
     /**
      * Handles an access denied failure.
@@ -23,12 +41,16 @@ class AccesDeniedHandler implements AccessDeniedHandlerInterface
      * @param Request $request
      * @param AccessDeniedException $accessDeniedException
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
 
-            $html = $twig->render('error403.html.twig');
-            return new Response($html,403);
+        $html = $this->twig->render('error403.html.twig');
+
+        return new Response($html,403);
 
     }
 }
--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -9,7 +9,42 @@
 namespace App\Security;
 
 
-class UserChecker
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
 {
 
+    /**
+     * Checks the user account before authentication.
+     *
+     * @param UserInterface $user
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof  User){
+            return;
+        }
+
+        if (!$user->getEnabled()){
+            $ex = new DisabledException('Votre compte a été désactivé');
+            $ex->setUser($user);
+            throw $ex;
+
+        }
+    }
+
+    /**
+     * Checks the user account after authentication.
+     *
+     * @param UserInterface $user
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
 }
